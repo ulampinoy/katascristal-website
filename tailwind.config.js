8075001c801fd5ac1f2dfb7c1195097f555cb0a4
Allan Reyes <chef@ulampinoy.com>
2025-03-04 00:03:52 +0100

    Apply font-family globally

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
         'text-light',
         {
             pattern: /(m|p)(t|b|l|r)-(0|px|1|1.5|2|2.5|3|3.5|4|5|6|7|8|9|10|11|12|14|16|20|24|28|32|36|40|44|48|52|56|60|64|72|80|96)/
-        },
+        }
     ],
     theme: {
         extend: {
@@ -23,7 +23,7 @@ module.exports = {
                 primary: themeStyle.primary
             },
             fontFamily: {
-                sans: ['Inter', 'sans-serif'],
+                sans: ['Nunito Sans', 'sans-serif'],
                 serif: ['Roboto Slab', 'serif']
             },
             gridTemplateColumns: {
